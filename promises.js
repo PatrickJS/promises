323ca945628cc2e23cc04ba6404389116cf72d5a
gdi2290 <github@gdi2290.com>
2013-08-22 12:17:50 -0700

    add changeState method to Promises

--- promises.js
+++ promises.js
@@ -1,5 +1,5 @@
 var Promises = function() {
-  // promises states
+  // promises state
   var State = {
     _PENDING:   0,
     _FULFILLED: 1,
@@ -7,6 +7,35 @@ var Promises = function() {
   };
 
   var Promises = {
-    state: State._PENDING
+    state: State._PENDING,
+    changeState: function(state, value)  {
+
+      // catch changing to state (perhaps trying to change value)
+      if (this.state === state) {
+        throw new Error('can\'t transition to same state: ' + state);
+      }
+
+      // trying to change out of fulfilled or rejected
+      if (this.state === State._FULFILLED || this.state === State._REJECTED) {
+        throw new Error('can\'t transition from current state: ' + state);
+      }
+
+      // if second argument is't given at all (passing undefined allowed)
+      if (state === State._FULFILLED && arguments.length < 2) {
+        throw new Error('transition to fulfilled must have a non \'null\' value');
+      }
+
+      // if  null reason is passed in
+      if (state === State._REJECTED && value === null) {
+        throw new Error('transition to rejected must have a non \'null\' reason');
+      }
+      // change state
+      this.state = state;
+      this.value = value;
+      return this.state;
+    }
+
+
   };
+
 };
