d77011f4f89a76cb0f343b1ca774ee697d867d5a
gdi2290 <github@gdi2290.com>
2013-08-22 12:08:29 -0700

    add state

--- promises.js
+++ promises.js
@@ -1,9 +1,12 @@
 var Promises = function() {
+  // promises states
   var State = {
-
+    _PENDING:   0,
+    _FULFILLED: 1,
+    _REJECTED:  2
   };
 
   var Promises = {
-
+    state: State._PENDING
   };
 };
